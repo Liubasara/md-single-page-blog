2eb50214e2874b53867c94db9c0d87c83bec2966
liubasara <followliu@163.com>
2021-06-30 22:18:55 +0800

    feat: 引入 gulp-markdown-to-json

--- gulpTasks/articleTasks.js
+++ gulpTasks/articleTasks.js
@@ -1,10 +1,14 @@
 const path = require('path')
+const markdownToJSON = require('gulp-markdown-to-json')
+const marked = require('marked')
+const gulp = require('gulp')
 
 const ARTICLE_PATH = process.env.blogArticlePath || path.resolve(__dirname, '..', 'article', 'demo')
+const TARGET_PATH = path.resolve(__dirname, '..', 'article', 'dist')
 
-function generateArticleToJsTask(cb) {
-  console.log(ARTICLE_PATH)
-  cb()
+function generateArticleToJsTask() {
+  const DEMO_MD_PATH = path.resolve(ARTICLE_PATH, 'tag1', 'demoBlog.md')
+  return gulp.src(DEMO_MD_PATH).pipe(markdownToJSON(marked)).pipe(gulp.dest(TARGET_PATH))
 }
 
 module.exports = {
