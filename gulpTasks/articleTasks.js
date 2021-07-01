3b6f6e94f3dd6b1e1427a10ae55b6c4a62ac67f3
liubasara <followliu@163.com>
2021-07-01 22:44:25 +0800

    fix: 修改图片和文件输出位置

--- gulpTasks/articleTasks.js
+++ gulpTasks/articleTasks.js
@@ -7,22 +7,24 @@ const getAllArticleImgPaths = require('./utils/getAllArticleImgPaths')
 const { generateDelTask } = require('./helperTasks')
 
 const TARGET_PATH = path.resolve(__dirname, '..', 'article', 'dist')
-const IMG_TARGET_PATH = path.resolve(TARGET_PATH, 'img')
 const ARTICLE_PATH = process.env.blogArticlePath || path.resolve(__dirname, '..', 'article', 'demo')
 const CLEAN_PATH = path.resolve(TARGET_PATH, '**')
 
 async function generateArticleToJsTask() {
   const allArticlePaths = await getAllArticlePaths(ARTICLE_PATH)
   console.log('getMarkdown', allArticlePaths)
-  return src(allArticlePaths).pipe(markdownToJSON(marked)).pipe(dest(TARGET_PATH))
+  return src(allArticlePaths, { base: ARTICLE_PATH }).pipe(markdownToJSON(marked)).pipe(dest(TARGET_PATH))
 }
 
 async function generateArticleImgTask() {
   const allArticleImgPaths = await getAllArticleImgPaths(ARTICLE_PATH)
   console.log('getMarkdownImage', allArticleImgPaths)
-  return src(allArticleImgPaths).pipe(dest(IMG_TARGET_PATH))
+  return src(allArticleImgPaths, { base: ARTICLE_PATH }).pipe(dest(TARGET_PATH))
 }
 
 module.exports = {
-  generateArticleToJsTask: series(generateDelTask(CLEAN_PATH), parallel(generateArticleToJsTask, generateArticleImgTask))
+  generateArticleToJsTask: series(
+    generateDelTask(CLEAN_PATH),
+    parallel(generateArticleToJsTask, generateArticleImgTask)
+  )
 }
