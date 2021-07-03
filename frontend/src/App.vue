e110eab45da4697fd6e113f8e7edf8486eebbf3a
liubasara <followliu@163.com>
2021-07-04 02:23:15 +0800

    feat: frontend load article example

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,35 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, onBeforeMount } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import directory from 'articleDist/directory/directory.json'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup() {
+    onBeforeMount(async () => {
+      console.log('\n')
+      console.log('directory', directory)
+      const { default: allContents } = await import('articleDist/allContents/allContents.json')
+      console.log('allContents', allContents)
+
+      // https://vitejs.dev/guide/features.html#glob-import vite 专有语法，打包固定文件
+      const modules = import.meta.glob('/../article/dist/allArticle/**/*.*')
+      const moduleKeys = Object.keys(modules)
+      console.log(modules)
+      directory.forEach(async <T extends { url: string }>(articleObj: T) => {
+        const articleModuleKey = moduleKeys.find(key => new RegExp(articleObj.url).test(key))
+        if (!articleModuleKey) return
+        const { default: article } = await modules[articleModuleKey]()
+        console.log(article)
+      })
+      console.log('\n')
+    })
+    
   }
 })
 </script>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const ROOT_PATH = path.resolve('..')
+const ARTICLE_DIST = path.resolve(ROOT_PATH, 'article', 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'articleDist': ARTICLE_DIST
+    }
+  },
+  // server: {
+  //   fsServe: {
+  //     strict: true,
+  //     root: ROOT_PATH
+  //   }
+  // }
 })
