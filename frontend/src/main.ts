a588a879f9bd3b4de90800ccaf97418f717d6114
liubasara <followliu@163.com>
2021-07-06 23:42:57 +0800

    feat: 引入 vuex4 && vue-router4

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,36 @@
 import { createApp } from 'vue'
-import App from './demoApp.vue'
+import App from './App.vue'
+import demoApp from './demoApp.vue'
+import { createStore } from 'vuex'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+interface stateInterface {
+  count: number;
+}
+
+const store = createStore<stateInterface>({
+  state () {
+    const _state: stateInterface = {
+      count: 0
+    }
+    return _state
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: demoApp }
+  ]
+})
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+
+app.mount('#app')
