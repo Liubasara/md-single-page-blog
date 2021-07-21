5b21f1955455102967a871a5e884e8139ddc52f0
liubasara <followliu@163.com>
2021-07-21 23:12:47 +0800

    fix: 解决性能函数类型丢失问题

--- frontend/src/utils/performanceTest.ts
+++ frontend/src/utils/performanceTest.ts
@@ -1,9 +1,23 @@
-function performanceTest<T extends Function>(fn: T) {
-  return function (this: any, ...args: any[]) {
+function performanceTest<T extends (...args: Array<any>) => any>(fn: T) {
+  type fnArguments<F extends (...args: Array<any>) => any> = F extends (
+    ...args: infer V
+  ) => any
+    ? V
+    : never
+  type fnReturn = ReturnType<T>
+  return function (this: any, ...args: fnArguments<T>): fnReturn {
     const t0 = performance.now()
     const result = fn.apply(this, args)
     const t1 = performance.now()
-    console.log('函数名称:', fn.name, '执行耗时', (t1 - t0).toFixed(4), '毫秒:', result)
+    console.log(
+      '函数名称:',
+      fn.name,
+      '执行耗时',
+      (t1 - t0).toFixed(4),
+      '毫秒:',
+      result
+    )
+    return result
   }
 }
 
