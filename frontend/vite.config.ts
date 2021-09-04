129af97d48d22f29d2311387d05d863e265236c7
liubasara <followliu@163.com>
2021-09-04 13:12:10 +0800

    feat: blog 根据环境变量自定义 blogTitle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import html from 'vite-plugin-html'
 
 const ROOT_PATH = path.resolve('..')
 const ARTICLE_DIST = path.resolve(ROOT_PATH, 'article', 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vueJsx(), vue()],
+  plugins: [
+    vueJsx(),
+    vue(),
+    html({
+      inject: {
+        data: {
+          blogTitle: process.env.blogTitle || 'blogTitle'
+        }
+      },
+      minify: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -18,7 +30,7 @@ export default defineConfig({
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
-  },
+  }
   // server: {
   //   fsServe: {
   //     strict: true,
